8f0c03fff00e66c37916428bc28d8795af2d1c26
vivgarcia <viv.garcia88@gmail.com>
2020-03-04 20:54:36 -0600

    updating psuedocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,17 +16,22 @@ for (let i = 0; i < topics.length; i++) {
 //add a new show 
 $("#addAShow").on("click", function(event){
     event.preventDefault();
+    // grab show title from input field
     var newShow = $("#showTitle").val().trim();
     console.log(newShow);
+    //push new show into the topics array
     topics.push(newShow);
+    // create new button and append it
     var newButton = $("<button>").text(newShow.toLowerCase());
     newButton.attr("data-show", newShow);
     newButton.addClass("btn btn-primary customButton");
     $(".buttonsGoHere").append(newButton);
+    // clear the input field
     $("#showTitle").val("");
 })
 
 $(document).ready(function(){
+    // create variable by grabbing data-show
     var show = $(this).attr("data-show");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=KkpY2QCGen77UrOTWYKKC98mbcNWeRTo&limit=10";
     $.ajax ({
