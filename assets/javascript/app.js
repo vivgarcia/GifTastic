65e2c3bcf9019af8847cb46917329c88650cbe1d
vivgarcia <viv.garcia88@gmail.com>
2020-03-04 20:43:40 -0600

    added functionality to buttons and to input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,39 @@
 //defining topics
 var topics = [
-    "bob belcher",
-    "linda belcher",
-    "tina belcher",
-    "gene belcher",
-    "louise belcher"
+    "bobs burgers",
+    "rick and morty",
+    "king of the hill",
+    "futurama",
+    "daria"
 ]
-//running a loop through the topics array to create a button, assign a data-character to each, add bootstrap classes and append to div
+//running a loop through the topics array to create a button, assign a data-show to each, add bootstrap classes and append to div
 for (let i = 0; i < topics.length; i++) {
-    var button = $("<button>");
-    button.attr("data-character", topics[i]);
-    button.addClass("btn btn-primary");
-    $("#buttonsGoHere").append(button);
+    var button = $("<button>").text(topics[i]);
+    button.attr("data-show", topics[i]);
+    button.addClass("btn btn-primary customButton");
+    $(".buttonsGoHere").append(button);
 }
+//add a new show 
+$("#addAShow").on("click", function(event){
+    event.preventDefault();
+    var newShow = $("#showTitle").val().trim();
+    console.log(newShow.toLowerCase());
+    topics.push(newShow);
+    var newButton = $("<button>").text(newShow);
+    newButton.attr("data-show", newShow);
+    newButton.addClass("btn btn-primary customButton");
+    $(".buttonsGoHere").append(newButton);
+})
 
 $(document).ready(function(){
-    var character = $(this).attr("data-character");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=KkpY2QCGen77UrOTWYKKC98mbcNWeRTo&limit=10";
-
+    var show = $(this).attr("data-show");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=KkpY2QCGen77UrOTWYKKC98mbcNWeRTo&limit=10";
     $.ajax ({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(queryURL);
         console.log(response);
-
     })
 
 })
